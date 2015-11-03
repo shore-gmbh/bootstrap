1a360b3d4988f9079c00fa4ff700d2b7826170ca
Lay Flags <git@mbfx.de>
2015-11-03 11:56:49 +0100

    add new build

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -2361,3 +2361,26 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
+
++function ($) {
+  'use strict';
+
+  //
+  // adds/removes the class 'form-control-has-focus' on a control label
+  // if the corresponding form control receives/misses focus
+  //
+  function handleFormControlHasFocusClassOnLabel(action) {
+    return function (evt) {
+      var $target = $(evt.target);
+
+      if (!$target.hasClass('form-control')) return;
+
+      $target.prev('.control-label')[action + 'Class']('form-control-has-focus');
+    }
+  }
+
+  $(document).on('focusin', handleFormControlHasFocusClassOnLabel('add'));
+  $(document).on('focusout', handleFormControlHasFocusClassOnLabel('remove'));
+
+}(jQuery);
+
