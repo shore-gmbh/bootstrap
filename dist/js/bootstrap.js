0916b58554a180c13e537a08ad881457eda06cc9
Lay Flags <git@mbfx.de>
2015-11-04 14:25:22 +0100

    add new build

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -2372,10 +2372,15 @@ if (typeof jQuery === 'undefined') {
   function handleFormControlHasFocusClassOnLabel(action) {
     return function (evt) {
       var $target = $(evt.target);
+      var $label;
 
       if (!$target.hasClass('form-control')) return;
+      if (!$target.attr('id')) return;
 
-      $target.prev('.control-label')[action + 'Class']('form-control-has-focus');
+      $label = $('label[for=' + $target.attr('id') + ']');
+      if (!$label.length) return;
+
+      $label[action + 'Class']('form-control-has-focus');
     }
   }
 
