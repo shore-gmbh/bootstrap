9adb382dd5eca913c62bb830b13742a406d13cce
Benjamin Bachhuber <benbachhuber@gmail.com>
2015-12-17 15:25:43 +0100

    add new build

--- dist/js/bootstrap.min.js
+++ dist/js/bootstrap.min.js
@@ -1,5 +1,5 @@
 /*!
- * Bootstrap v3.3.5-shore.18 (http://getbootstrap.com)
+ * Bootstrap v3.3.5-shore.19 (http://getbootstrap.com)
  * Copyright 2011-2015 Twitter, Inc.
  * Licensed under the MIT license
  */
